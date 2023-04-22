84cf1342850d62fbd012d2da575bce1f5e45da7a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-22 14:57:04 +0900

    a

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
+console.log("hello" + myName);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
