440ccbb1114162c76110aa452f29d2da08c20549
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-05-04 10:40:28 +0900

    deleting to dos

--- js/todo.js
+++ js/todo.js
@@ -12,7 +12,10 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo;
   const button = document.createElement("button");
-  button.innerText = "";
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
